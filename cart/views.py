fbe085bec7fc98dcb2575ff5e71bf18f90686025
amit238 <amit238@hotmail.co.uk>
2020-07-28 18:19:52 +0000

    added add post to my blog page

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
 
 # Create your views here.
 
@@ -6,3 +6,16 @@ def view_cart(request):
     """ A view that renders the cart page """
 
     return render(request, 'cart/cart.html')
+
+def add_to_cart(request, item_id):
+    """ Add plan to shopping cart """
+    
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    cart = request.session.get('cart', {})
+
+    request.session['cart'] = cart
+    return redirect(redirect_url)
+    
+
+    
