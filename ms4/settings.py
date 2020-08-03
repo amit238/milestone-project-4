f1fbb1b033219428ef88be242c1b340e9d13e303
amit238 <amit238@hotmail.co.uk>
2020-08-03 18:28:40 +0000

    completed stripe payment

--- ms4/settings.py
+++ ms4/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -115,11 +116,16 @@ WSGI_APPLICATION = 'ms4.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.parse(postgres://whvmsfxfmwtfgw:c79c620ef4da828730a42ccd1c5126b5ac5fb2513796284e0da9b4f4a1190840@ec2-54-247-103-43.eu-west-1.compute.amazonaws.com:5432/d7if93rfoc0fdj)
+    
 }
 
 
