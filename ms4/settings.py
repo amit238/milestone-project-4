424f026ad5f7d1767ee78f2f9dacad17d3554a48
amit238 <amit238@hotmail.co.uk>
2020-08-25 03:29:33 +0000

    Changed Default from email

--- ms4/settings.py
+++ ms4/settings.py
@@ -211,7 +211,7 @@ STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
 if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-    DEFAULT_FROM_EMAIL = 'FitnessFreaks@example.com'
+    DEFAULT_FROM_EMAIL = 'FitnessFreaks@FitnessFreaks.com'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
