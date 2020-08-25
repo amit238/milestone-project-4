5b5c182064fded5596731c6b47ce1ab70e49d363
amit238 <amit238@hotmail.co.uk>
2020-08-25 03:21:35 +0000

    Fixed indentation in settings

--- ms4/settings.py
+++ ms4/settings.py
@@ -209,7 +209,7 @@ STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
  # Email Verification
 
- if 'DEVELOPMENT' in os.environ:
+if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
     DEFAULT_FROM_EMAIL = 'FitnessFreaks@example.com'
 else:
